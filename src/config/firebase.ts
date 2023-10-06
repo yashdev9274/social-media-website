3e05a935d5bd2aff4460b36345ef78a60a7efe5e
yashdev9274 <yashdev.yvd@gmail.com>
2023-10-06 17:51:01 +0530

    connected ro db of firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider } from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +22,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,6 +1,9 @@
 import {useForm} from "react-hook-form";
 import * as yup from "yup"; 
 import {yupResolver} from "@hookform/resolvers/yup";
+import {addDoc, collection  } from "firebase/firestore";
+import {auth, db} from "../../config/firebase";
+import {useAuthState} from "react-firebase-hooks/auth"; 
 
 interface CreateFormData {
     title: string;
@@ -8,6 +11,8 @@ interface CreateFormData {
 }
 
 export const CreateForm = () => {
+
+    const [user]  = useAuthState(auth);
 
     const schema = yup.object().shape({
 
@@ -20,9 +25,19 @@ export const CreateForm = () => {
     });
 
 
-    const onCreatePost = (data: CreateFormData) => {
+    const postRef = collection(db, "posts");
 
-        console.log(data);
+    const onCreatePost = async (data: CreateFormData) => {
+
+        await addDoc(postRef, {
+
+            // title: data.title,
+            // description: data.description,
+            // we aliminated this above part with this
+            ...data,
+            user: user?.displayName,
+            id: user?.uid,
+        });
     };
 
     return (

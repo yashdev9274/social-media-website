7f1b7b32254fc44b62c8cf0a8215733d2437ee42
yashdev9274 <yashdev.yvd@gmail.com>
2023-10-06 13:52:55 +0530

    firebase install

---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzr1Hlj_pFgzmjbzbZc2DWyEbsxktFDLk",
+  authDomain: "social-media-website-8bba1.firebaseapp.com",
+  projectId: "social-media-website-8bba1",
+  storageBucket: "social-media-website-8bba1.appspot.com",
+  messagingSenderId: "954221737189",
+  appId: "1:954221737189:web:7dae047bd6ae17a7ba500c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,17 @@
+import {auth, provider} from "../config/firebase";
+import {signInWithPopup} from 'firebase/auth';
+
 export const Login = () => {
-    return <div>Login Page</div>;
+
+    const signInWithGoogle = async () => {
+
+        const result = await signInWithPopup(auth, provider);
+
+    }
+    return (
+        <div>
+            <p>Sign in with google to continue</p>
+            <button onClick = {signInWithGoogle}>Sign in with Google</button>
+        </div>
+    );
 };

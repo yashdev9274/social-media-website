911637090af6fb3301bc3106c864aef53a93dd34
yashdev9274 <yashdev.yvd@gmail.com>
2023-10-06 14:05:09 +0530

    auth update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,16 @@
 import {Link} from 'react-router-dom';
+import {auth} from '../config/firebase';
 
 export const Navbar = () => {
     return (
         <div>
             <Link to="/">Home</Link>
             <Link to="/Login">Login</Link>
+
+            <div>
+                <p> {auth.currentUser?.displayName} </p>
+                <img src ={auth.currentUser?.photoURL || ""} width="100" height="100"/>
+            </div>
 
         </div>
     );
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,16 @@
 import {auth, provider} from "../config/firebase";
 import {signInWithPopup} from 'firebase/auth';
+import {useNavigate} from 'react-router-dom';
 
 export const Login = () => {
+
+    const navigate = useNavigate();
 
     const signInWithGoogle = async () => {
 
         const result = await signInWithPopup(auth, provider);
+        console.log(result);
+        navigate('/');
 
     }
     return (

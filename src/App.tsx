0719dfc8a6a517cfb394894687439d7da22c446a
yashdev9274 <yashdev.yvd@gmail.com>
2023-10-06 12:32:22 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router , Route, Routes} from 'react-router-dom';
 import './App.css';
+import {Main} from './pages/main';
+import {Login} from './pages/login';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path = "/" element = {<Main/>}/>
+          <Route path = "/login" element = {<Login/>}/>
+
+        </Routes>
+        </Router>
+    </div>
   );
 }
 
